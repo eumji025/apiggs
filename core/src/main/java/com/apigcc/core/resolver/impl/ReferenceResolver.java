1091491b20600f5c9a1a6083651a20af30d07a4f
eumji025 <eumji025@gmail.com>
2019-04-05 13:29:02 +0800

    解决由于javapaser的bug，导致泛型只能匹配第一个的问题

--- core/src/main/java/com/apigcc/core/resolver/impl/ReferenceResolver.java
+++ core/src/main/java/com/apigcc/core/resolver/impl/ReferenceResolver.java
@@ -7,6 +7,7 @@ import com.github.javaparser.resolution.types.ResolvedReferenceType;
 import com.github.javaparser.resolution.types.ResolvedType;
 import com.github.javaparser.utils.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class ReferenceResolver extends Resolver {
@@ -15,11 +16,26 @@ public abstract class ReferenceResolver extends Resolver {
         return super.accept(resolvedType) && resolvedType.isReferenceType() && accept(resolvedType.asReferenceType().getTypeDeclaration());
     }
 
+    /**
+     * 修复因为 referenceType.getTypeParametersMap 获取到的pair的key值永远都是default值导致的问题，目前已提交pr给javaPaser
+     * <p>
+     * 临时适配方案
+     *
+     * @param types
+     * @param resolvedType
+     */
     @Override
     public void resolve(Types types, ResolvedType resolvedType) {
         ResolvedReferenceType referenceType = resolvedType.asReferenceType();
         ResolvedReferenceTypeDeclaration typeDeclaration = referenceType.getTypeDeclaration();
-        List<Pair<ResolvedTypeParameterDeclaration, ResolvedType>> typeParametersMap = referenceType.getTypeParametersMap();
+        //List<Pair<ResolvedTypeParameterDeclaration, ResolvedType>> typeParametersMap = referenceType.getTypeParametersMap();
+        List<Pair<ResolvedTypeParameterDeclaration, ResolvedType>> typeParametersMap = new ArrayList<>();
+        if (!referenceType.isRawType()) {
+            List<ResolvedType> resolvedTypes = referenceType.typeParametersValues();
+            for (int i = 0; i < typeDeclaration.getTypeParameters().size(); i++) {
+                typeParametersMap.add(new Pair<>(typeDeclaration.getTypeParameters().get(i), resolvedTypes.get(i)));
+            }
+        }
         resolve(types, typeDeclaration, typeParametersMap);
     }
 
