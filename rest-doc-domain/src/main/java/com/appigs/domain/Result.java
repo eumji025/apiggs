9a5db16ef412d325cd0fbda00b89e1297bb811db
eumji025 <eumji025@gmail.com>
2019-12-02 22:46:41 +0800

    subproject dependency module

---
+++ rest-doc-domain/src/main/java/com/appigs/domain/Result.java
@@ -0,0 +1,35 @@
+package com.appigs.domain;
+
+
+public class Result<T> {
+    private String code;
+
+    private String name;
+
+    private T data;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
